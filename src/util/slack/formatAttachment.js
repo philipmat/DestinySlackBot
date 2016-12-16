12c9bad32ce503e693737811037fef3e96c30a84
Danny <danny@dannymiller.me>
2016-12-16 11:27:34 -0500

    * Implement destiny player info command

--- src/util/slack/formatAttachment.js
+++ src/util/slack/formatAttachment.js
@@ -1,30 +1,8 @@
 export default formatAttachment;
 
-function formatAttachment({actions, thumb_url, title = '', title_link, text = '', fields = [], fallback, color, footer, mrkdwn_in = ["text", "pretext", "title"]}) {
-    let attachment = {
-        title,
-        text,
-        fields,
-        mrkdwn_in
-    };
-
-    if(color) {
-        attachment['color'] = color;
-    }
-    if(actions) {
-        attachment['actions'] = actions;
-    }
-    if(title_link) {
-        attachment['title_link'] = title_link;
-    }
-    if(fallback) {
-        attachment['fallback'] = fallback;
-    }
-    if(footer) {
-        attachment['footer'] = footer;
-    }
-    if(thumb_url) {
-        attachment['thumb_url'] = thumb_url;
+function formatAttachment(attachment = {}) {
+    if(!attachment.mrkdwn_in) {
+        attachment.mrkdwn_in = ["text", "pretext", "title", "fields"]
     }
 
     return attachment;
